5589ee3fea1d0f90cadfdd8661e4c27471e917e9
Sashi1060 <sashi286y@gmail.com>
2026-04-14 09:08:24 +0530

    changed settings.py

--- vasavi_project/settings.py
+++ vasavi_project/settings.py
@@ -57,6 +57,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -166,7 +167,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_SOURCE_DIR = BASE_DIR / 'static'
+if STATIC_SOURCE_DIR.exists():
+    STATICFILES_DIRS = [STATIC_SOURCE_DIR]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
